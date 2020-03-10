18a97b4a0d85f7f0aaf597c34173dbad974099d6
Pavel Kohal <pavelkohal@gmail.com>
2020-03-10 15:55:29 +0300

    add refactored part of the third webdriver task

---
+++ stage2/src/test/java/by/epamtc/automation/selenium/task3/test/ThirdTaskTest.java
@@ -0,0 +1,24 @@
+package by.epamtc.automation.selenium.task3.test;
+
+import by.epamtc.automation.selenium.task3.page.GoogleCloudMainPageTask3;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ThirdTaskTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        new GoogleCloudMainPageTask3(driver)
+                .openHomePage()
+                .fillInSearchInputLine()
+                .selectDesiredSearchResult()
+                .fillInNumberOfInstance()
+                .selectOperationSystem();
+
+
+        Thread.sleep(10000);
+        driver.quit();
+    }
+
+
+}
